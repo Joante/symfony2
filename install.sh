0c401e4cc2943a78ea210eeb8d22be79b4b98c9b
joante <joanteich@gmail.com>
2018-12-17 10:24:38 -0300

    Arreglo de Install.sh 2

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #1/bin/bash
-/var/www/html/joan/composer install
-/var/www/html/app/console doctrine:database:drop --force
-/var/www/html/app/console doctrine:database:create
-/var/www/html/app/console doctrine:schema:update --force
-/var/www/html/app/console cache:clear 
+sudo /var/www/html/joan/composer install
+sudo /var/www/html/joan/app/console doctrine:database:drop --force
+sudo /var/www/html/joan/app/console doctrine:database:create
+sudo /var/www/html/joan/app/console doctrine:schema:update --force
+sudo /var/www/html/joan/app/console cache:clear 
